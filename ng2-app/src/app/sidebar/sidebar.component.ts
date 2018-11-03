d0a9a6722d1f23f641bd08e66aff0c3e114d9711
Naresh <naresh.adepu@vertiv.com>
2018-11-04 02:39:59 +0530

    WDM

---
+++ ng2-app/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import { 
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Optional,
+  Output,
+  SimpleChanges,
+  ViewChild
+ } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  private _opened: boolean = false;
+
+  private _toggleSidebar() {
+    this._opened = !this._opened;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
